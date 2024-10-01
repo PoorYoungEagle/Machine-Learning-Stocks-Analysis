b996645ea873736c50c85221980983e0f202328f
Jeb <jebastonjoseph@gmail.com>
2024-10-01 20:31:00 +0530

    Add files via upload

--- CraneML.py
+++ CraneML.py
@@ -1,9 +1,4 @@
-import pandas as pd
-import requests_html
-from lxml.html.clean import Cleaner
-import numpy as np
 from matplotlib import pyplot as plt
-import math
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
@@ -11,11 +6,18 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
 from sklearn.model_selection import train_test_split
-import random
 from yahoo_fin import stock_info as si
 from sklearn.linear_model import LinearRegression
 import pandas_ta as ta
 
+'''
+
+Main code for Machine Learning
+Uses indicators and Linear Regression to learn the data
+
+'''
+
+#Inputs given ticker and returns data associated to that stock
 def ticker(ticker="NVDA"):
   df = si.get_data(ticker)
   open_price = df['open']
@@ -30,19 +32,18 @@ def ticker(ticker="NVDA"):
   high_prices = df['high']
   low_prices = df['low']
 
-  close_prices = close_prices[len(
-      close_prices) - DATA_LEN:len(close_prices)].to_list()
-  open_prices = open_prices[len(open_prices) -
-                            DATA_LEN:len(open_prices)].to_list()
+  close_prices = close_prices[len(close_prices) - DATA_LEN:len(close_prices)].to_list()
+  open_prices = open_prices[len(open_prices) - DATA_LEN:len(open_prices)].to_list()
   volumes = volumes[len(volumes) - DATA_LEN:len(volumes)].to_list()
-  high_prices = high_prices[len(high_prices) -
-                            DATA_LEN:len(high_prices)].to_list()
+  high_prices = high_prices[len(high_prices) - DATA_LEN:len(high_prices)].to_list()
   low_prices = low_prices[len(low_prices) - DATA_LEN:len(low_prices)].to_list()
 
   return df, open_prices, close_price, close_prices, high_prices, low_prices, volumes
+
 DATA_LEN = 300
 df, open_prices, close_price, close_prices, high_prices, low_prices, volumes = ticker()
 
+#Just plots the open prices
 def plot_open_prices(open_prices):
   plt.style.use('dark_background')
   plt.figure(figsize=(10, 6))
@@ -55,6 +56,7 @@ def plot_open_prices(open_prices):
   return plt.gcf()
 plot_open_prices(open_prices)
 
+#Just plots the close prices
 def plot_close_prices(close_prices):
   plt.figure(figsize=(18, 5))
   plt.title("Close prices", fontsize=15)
@@ -69,15 +71,17 @@ def plot_close_prices(close_prices):
   return plt.gcf()
 plot_close_prices(close_prices)
 
-close_for_calc = df['close']
-close_for_calc = close_for_calc[len(
-    close_for_calc) - DATA_LEN:len(close_for_calc)]
 
 model = LinearRegression()
+
+#Placeholder function which is to be exported from Cranelit.py
 def Progress_Update(Placeholder):
     pass
+
+#Main Machine Learning code function using Linear Regression model
 def MLModel(Progress_Update, epochs=100):
 
+  #Uses RSI, EMA and SMA indicators. Other indicators can be brought in by adding another pandas_ta line with the indicator and then placing the name into the 'X' pandas dataframe
   df['RSI'] = ta.rsi(close_price, length=14)
   df['EMA'] = ta.ema(close_price, length=14)
   df['SMA'] = ta.sma(close_price, length=14)
@@ -87,10 +91,12 @@ def MLModel(Progress_Update, epochs=100):
   df['Target'] = (close_price.shift(-1) > close_price).astype(int)
 
   X=df[['RSI','EMA','SMA']].dropna()
+  print(type(X))
   Y=df['Target'].loc[X.index]
   scaler = MinMaxScaler()
   X_scaled = scaler.fit_transform(X)
 
+  #Machine Learning Code
   X_train, X_test, Y_train, Y_test = train_test_split(X_scaled, Y, test_size=0.2, random_state=1)
   model = tf.keras.Sequential([
       tf.keras.layers.Dense(256, input_shape=(X_train.shape[1],), activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01)),
@@ -109,23 +115,34 @@ def MLModel(Progress_Update, epochs=100):
   optimizer = Adam(learning_rate=lr_schedule,clipvalue=1.0)
   model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
+  #Progress slider for streamlit
+  Accuracy = []
+  Val_Accuracy = []
+
   for epoch in range(epochs):
     history=model.fit(X_train, Y_train, epochs=1, validation_data=(X_test, Y_test))
+
+    Accuracy.extend(history.history['accuracy'])
+    Val_Accuracy.extend(history.history['val_accuracy'])
+
     progress = (epoch + 1) / epochs
     Progress_Update(progress)
 
+  #Plotting the accuracy on the graph with relation to Epoch and Accuracy
   loss,accuracy = model.evaluate(X_test, Y_test)
   print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.2f}")
-  plt.plot(history.history['accuracy'], label='Accuracy')
-  plt.plot(history.history['val_accuracy'], label = 'Validation Accuracy')
+  plt.plot(Accuracy, label='Accuracy')
+  plt.plot(Val_Accuracy, label='Validation Accuracy')
   plt.xlabel('Epoch')
   plt.ylabel('Accuracy')
   plt.ylim([0, 1])
+  plt.xlim([1,epochs])
   plt.legend(loc='lower right')
   plt.show()
   return plt.gcf(), loss, accuracy, history
 
 Placehold,loss,accuracy,history = MLModel(Progress_Update,1)
+
 """def Accuracy_Graph():
     print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.2f}")
     plt.plot(history.history['accuracy'], label='Accuracy')
--- Cranelit.py
+++ Cranelit.py
@@ -4,30 +4,51 @@ from CraneML import plot_close_prices
 from CraneML import plot_open_prices
 from CraneML import MLModel
 
-st.title('Crane Crap')
-st.subheader("Ticker")
-options = ['NVDA','AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA']
-selected_stock = st.selectbox('Choose a stock:', options)
 
-df, open_prices, close_price, close_prices, high_prices, low_prices, volumes = ticker(selected_stock)
-st.subheader("Open Prices Plot")
-Plot_Open = plot_open_prices(open_prices)
-st.pyplot(Plot_Open)
+#The main file which uses streamlit to provide a user interface for the models.
+#Run using "streamlit run Cranelit.py" in the terminal.
 
-st.subheader("Close Prices Plot")
-Plot_Close = plot_close_prices(close_prices)
-st.pyplot(Plot_Close)
 
-st.subheader("Machine Learning")
-epochs = st.slider('Select number of epochs:', min_value=1, max_value=2000, value=100)
-progress_bar = st.empty()
-def Progress_Update(progress):
-      progress_bar.progress(progress)
-loss=0.0
-accuracy=0.0
-if st.button('Start Training'):
-  st.session_state.training = True
-  AccuracyG,loss,accuracy,history=MLModel(Progress_Update, epochs)
-  st.success("Training completed!")
-  st.write(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.2f}")
-  st.pyplot(AccuracyG)
+def main():
+      
+      #Provides options for the type of stocks
+      #Can be changed by adding in more tickers into the 'options' list
+      st.title('Crane')
+      st.subheader("Ticker")
+      options = ['NVDA','AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA']
+      selected_stock = st.selectbox('Choose a stock:', options)
+
+      #Returns the values required to add in the visual representation of Open Prices, Close Prices and Accuracy Graph
+      df, open_prices, close_price, close_prices, high_prices, low_prices, volumes = ticker(selected_stock)
+
+      #Open Prices
+      st.subheader("Open Prices Plot")
+      Plot_Open = plot_open_prices(open_prices)
+      st.pyplot(Plot_Open)
+      
+      #Close Prices
+      st.subheader("Close Prices Plot")
+      Plot_Close = plot_close_prices(close_prices)
+      st.pyplot(Plot_Close)
+
+      #Streamlit Slider with a maximum of 2000 epochs
+      st.subheader("Machine Learning")
+      epochs = st.slider('Select number of epochs:', min_value=1, max_value=2000, value=100)
+
+      #Progress bar for completion
+      progress_bar = st.empty()
+      def Progress_Update(progress):
+            progress_bar.progress(progress)
+      loss=0.0
+      accuracy=0.0
+      
+      #Button to activate the machine learning code
+      if st.button('Start Training'):
+            st.session_state.training = True
+            AccuracyG,loss,accuracy,history=MLModel(Progress_Update, epochs)
+            st.success("Training completed!")
+            st.write(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.2f}")
+            st.pyplot(AccuracyG)
+
+if __name__ == "__main__":
+      main()
